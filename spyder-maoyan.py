7c6e889760d66035c9eb6dfae6f3aaf93c677ca9
BINGSSJ <1291930177@qq.com>
2018-11-12 23:04:53 +0800

    01

---
+++ spyder-maoyan.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 12 19:44:55 2018
+
+@author: bing
+"""
+#spyder maoyan
+import requests
+import re
+headers={
+        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
+        }
+url='http://www.maoyan.com/board/4'
+def get_page(url):
+    response=requests.get(url,headers=headers)
+    if response.status_code==200:
+        return response.text
+    print(response.status_code)
+    return None
+def parse_page(html):
+    pattern=re.compile(
+            '<dd>.*?board-index.*?>(.*?)</i>.*?board-img.*?src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+            re.S)
+    items=re.findall(pattern,html)
+    print(items)
+    for item in items:
+        yield{
+                'index':item[0],
+                'image-url':item[1],
+                'title':item[2].strip(),
+                'actor':item[3].strip()[3:] if(len(item[3])>3) else'',
+                'time':item[4].strip()[5:] if(len(item[4])>5) else '',
+                'score':item[5].strip()+item[6].strip()
+                }
+        print(item)
+        
+if __name__=='__main__':
+    html=get_page(url)
+    parse_page(html)
