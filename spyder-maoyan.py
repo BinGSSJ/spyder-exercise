7e36fd6bdf01859d7dd62cbe9cb82a95c714ad9a
BINGSSJ <1291930177@qq.com>
2018-11-13 11:37:05 +0800

    spyder-maoyan

--- spyder-maoyan.py
+++ spyder-maoyan.py
@@ -8,22 +8,25 @@ Created on Mon Nov 12 19:44:55 2018
 #spyder maoyan
 import requests
 import re
+import json
+import time
 headers={
         'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
         }
-url='http://www.maoyan.com/board/4'
 def get_page(url):
     response=requests.get(url,headers=headers)
     if response.status_code==200:
         return response.text
     print(response.status_code)
     return None
+def write_to_file(content):
+    with open('resuly.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(content,ensure_ascii=False)+'\n')
 def parse_page(html):
     pattern=re.compile(
-            '<dd>.*?board-index.*?>(.*?)</i>.*?board-img.*?src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
+            '<dd>.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>(.*?)</p>.*?releasetime.*?>(.*?)</p>.*?integer.*?>(.*?)</i>.*?fraction.*?>(.*?)</i>.*?</dd>',
             re.S)
     items=re.findall(pattern,html)
-    print(items)
     for item in items:
         yield{
                 'index':item[0],
@@ -33,8 +36,16 @@ def parse_page(html):
                 'time':item[4].strip()[5:] if(len(item[4])>5) else '',
                 'score':item[5].strip()+item[6].strip()
                 }
+     
+        
+def main(offset):
+    url='http://www.maoyan.com/board/4?offset='+str(offset)
+    html=get_page(url)
+    for item in parse_page(html):
         print(item)
+        write_to_file(item)
         
 if __name__=='__main__':
-    html=get_page(url)
-    parse_page(html)
+    for i in range(10):
+        main(offset=i*10)
+        time.sleep(1)
